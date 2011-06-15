129d25c721804feffde8234505290f7ad1566a18
frederik.vernelen@gmail.com <frederik.vernelen@gmail.com@6277a232-4167-11de-998b-8fedbbe774c7>
2011-06-15 15:01:45 +0000

    3_5_6 release 06/2011

---
+++ tags/3_5_6/eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+DIRNAME=`dirname $0`
+
+echo $$
+
+IFS=$'\n' ver=($(sw_vers -productVersion))
+
+if [[ ($ver == *10.5*) && ($ver < "10.5.6") ]]; then
+		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target /
+    else if [[ $ver == *10.5* || $ver == *10.6* ]]; then
+            /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11.mpkg -target /
+            /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
+	fi
+fi
