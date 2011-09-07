5c7cc4a2945959a6f4de971a94184444fdd0f5ff
frederik.vernelen <frederik.vernelen@6277a232-4167-11de-998b-8fedbbe774c7>
2011-09-07 14:55:21 +0000

    added new ACR drivers for mac OS X 10.7

--- eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -10,8 +10,12 @@ if [[ ($ver == *10.5*) && ($ver < "10.5.6") ]]; then
 		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
         /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target /
         /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target /
-    else if [[ $ver == *10.5* || $ver == *10.6* || $ver == *10.7* ]]; then
+    else if [[ $ver == *10.5* || $ver == *10.6* ]]; then
             /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11.mpkg -target /
             /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
+	fi
+    else if [[ $ver == *10.7* ]]; then
+            /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11_10.7.mpkg -target /
+            /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.7.mpkg -target /
 	fi
 fi
