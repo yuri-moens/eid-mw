7385b91449b0adde6afef50c18be09bfc7961757
frederik.vernelen <frederik.vernelen@6277a232-4167-11de-998b-8fedbbe774c7>
2013-03-06 09:18:48 +0000

    405 branch to backport the pkcs11 locking fix from trunk

---
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+DIRNAME=`dirname $0`
+
+#echo the script's pid
+echo $$
+
+IFS=$'\n' ver=($(sw_vers -productVersion))
+
+if [[ ($ver == *10.5*) && ($ver < "10.5.6") ]]; then
+		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target /
+    else if [[ $ver == *10.5* || $ver == *10.6* ]]; then
+            /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11.mpkg -target /
+            /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
+		else if [[ $ver == *10.7* || $ver == *10.8* ]]; then
+				/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11_10.7.mpkg -target /
+				/usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.7.mpkg -target /
+		fi
+	fi
+fi
